160c4ea65cc00d084ec29b33661f579286ed5ab2
EarlyAnt <54763755@qq.com>
2021-10-27 12:00:28 +0800

    1.已使用WKWebView替换UIWebView并打开百度首页。

--- WebViewDemo/WebViewDemo/ViewController.swift
+++ WebViewDemo/WebViewDemo/ViewController.swift
@@ -5,26 +5,24 @@
 //  Created by gaobo on 2021/10/27.
 //
 
-import UIKit
+import WebKit
 
 class ViewController: UIViewController, UIActionSheetDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        /*
-        // Do any additional setup after loading the view, typically from a nib.
-        let rect = CGRect(x: 0, y: 20, width: 360, height: 720)//创建一个现实区域
-        let webView = UIWebView(frame: rect)//初始化网页视图对象，并设置其位置和尺寸
-        */
+        let webConfiguration = WKWebViewConfiguration()
         
-        var screenBounds:CGRect = UIScreen.main.bounds;
-        let webView = UIWebView(frame: screenBounds)//初始化网页视图对象，并设置其位置和尺寸
-        
-        let url = URL(string: "https://www.baidu.com")//创建一个网络地址对象
-        let request = URLRequest(url: url!)//创建一个网址请求对象，作为网页视图对象的网络请求
-        
-        webView.loadRequest(request)//使用网页视图对象加载设置的网址
+        //创建WebView
+        let webView = WKWebView(frame: view.bounds, configuration: webConfiguration)
+        //创建网址
+        let url = URL(string: "http://www.baidu.com")
+        //创建请求
+        let request = URLRequest(url: url!)
+        //加载请求
+        webView.load(request)
+        //添加WebView到当前页面
         self.view.addSubview(webView)//将网页视图对象添加到当前视图控制器的根视图
     }
     
