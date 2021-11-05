b4df7daac3cc20741316d396803f2e7b77dad0ea
EarlyAnt <54763755@qq.com>
2021-11-05 10:38:46 +0800

    1.将UnityGame加入库中。

---
+++ UnityGame/Assets/Scripts/SimpleGame.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class SimpleGame : MonoBehaviour
+{
+    [SerializeField]
+    private Transform cubeObject;
+    [SerializeField]
+    private Vector3 angle;
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        this.cubeObject.Rotate(this.angle);
+    }
+
+    public void ExitGame()
+    {
+        Application.Unload();
+        Debug.Log("<><SimpleGame.ExitGame>unload scene");
+    }
+}
