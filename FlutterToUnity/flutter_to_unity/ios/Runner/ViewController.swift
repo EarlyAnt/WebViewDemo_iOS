67af30fe040b410cb1deff5945af2e04a4526d62
EarlyAnt <54763755@qq.com>
2021-11-10 18:29:38 +0800

    1.ViewController.swift，在导航条上加上了按钮，简单实现了返回上个页面的功能。

--- FlutterToUnity/flutter_to_unity/ios/Runner/ViewController.swift
+++ FlutterToUnity/flutter_to_unity/ios/Runner/ViewController.swift
@@ -10,6 +10,7 @@ import WebKit
 import SwiftUI
 
 class ViewController: UIViewController {
+    var navigationBar:UINavigationBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,9 +37,48 @@ class ViewController: UIViewController {
         //添加WebView到当前页面
         self.view.addSubview(webView)//将网页视图对象
         
+        //实例化当行条
+        navigationBar = UINavigationBar(frame: CGRect(x: 0, y: 20, width: self.view.bounds.width, height: 44))
+        //添加导航项
+        navigationBar.pushItem(onMakeNavitem(), animated: true)
+        self.view.addSubview(navigationBar)
 
         print("lucky")
     }
+
+    func onMakeNavitem()->UINavigationItem{
+        //创建导航项
+        let navigationitem = UINavigationItem()
+        let leftBtn = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.cancel, target: self, action: #selector(leftButtonClick))
+        //let rightBtn = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.cancel, target: self, action: #selector(rightButtonClick))
+        //navigationitem.title = "第1导航栏"
+        navigationitem.setLeftBarButton(leftBtn, animated: true)
+        //navigationitem.setRightBarButton(rightBtn, animated: true)
+        return navigationitem
+    }
+    @objc func leftButtonClick(){
+        /*
+        count = count + 1
+        //更新内容
+        navigationBar.pushItem(onMakeNavitem(), animated: true)
+         */
+        print("previous")
+        
+        self.dismiss(animated: true, completion: nil)
+        Unity.shared.show()
+    }
+    /*
+    @objc func rightButtonClick(){
+        /*
+        if count > 1{
+            count -= 1
+            //返回上一层
+            navigationBar.popItem(animated: true)
+        }
+        */
+        print("next")
+    }
+    */
 
 }
 
